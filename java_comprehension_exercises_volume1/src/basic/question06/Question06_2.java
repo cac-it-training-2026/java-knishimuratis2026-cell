42d17d824a775c79b9da59d0046c1600583f9483
西村和眞（NISHIMURAKazuma <NISHIMURAKazuma@192.168.0.86>
2026-04-16 13:51:58 +0900

    6-01,02

--- java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
+++ java_comprehension_exercises_volume1/src/basic/question06/Question06_2.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 public class Question06_2 {
 
 	public static void main(String[] args) throws IOException {
+		String[] studentName = { "かなこ", "ゆうた", "あい", "はるひ", "かずき" };
+		for (int i = 0; i < studentName.length; i++) {
+			System.out.println(studentName[i]);
+		}
 
 		// TODO:ここに実装
-		
+
 	}
 }
